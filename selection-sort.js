d27b19f5d1886cddda1e49ad53c0130bd7d5ecbc
ismail <mismailyusuf@gmail.com>
2018-07-22 21:46:45 +0700

    fix

--- selection-sort.js
+++ selection-sort.js
@@ -1,21 +1,24 @@
 function selectionSort (numArr) {
-    var temp = 0;
-    for (var i = 0; i < 1; i++) {
-        for (var j = 1; j < numArr.length; j++) {
+    for (var i = 0; i < numArr.length - 1; i++) {
+        var temp = 0;
+        var k = 0;
+        for (var j = i + 1; j < numArr.length; j++) {
             if (numArr[j] < numArr [i]) {
                 if (temp === 0) {
                     temp = numArr[j];
+                    k = j;
                 } else if (numArr[j] < temp) {
                     temp = numArr[j];
+                    k = j;
                 }
             }
         }
         if (temp !== 0) {
-            numArr[j-1] = numArr[i];
+            numArr[k] = numArr[i];
             numArr[i] = temp;
         }
     }
     return numArr;
 }
 
-console.log(selectionSort([5, 3, 7, 2]))
+console.log(selectionSort([5, 3, 7, 2, 6, 10]));
